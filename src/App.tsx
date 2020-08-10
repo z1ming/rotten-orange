38febbcdb1ce65cc615a2cb6d65958e9ab52f21f
jamie <mzm@mail.dlue.edu.cn>
2020-08-10 11:18:37 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Button, { ButtonType, ButtonSize } from './components/Button/button'
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button className="custom"> Hello </Button>
+        <Button disabled> Disabled Button </Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}> Large Primary </Button>
+        <Button btnType={ButtonType.Danger} size={ButtonSize.Small}> Small Danger </Button>
+        <Button btnType={ButtonType.Link} href="http://www.baidu.com" target="_blank"> Baidu Link </Button>
+        <Button btnType={ButtonType.Link} href="http://www.baidu.com" disabled> Disabled Link </Button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
